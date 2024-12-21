77584b8e850c275f8674cf769bed081a86b38c64
Nick Nepochatyy <nnepochatyy@mail.ru>
2024-12-21 09:54:30 +0500

    push service web interface

--- push/run.py
+++ push/run.py
@@ -1,6 +1,11 @@
 from push.app import create_app
+from flask import render_template
 
 app = create_app()
+
+@app.route('/push')
+def push():
+    return render_template('push_service.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
