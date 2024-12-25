60edef5fa6de0fbb1ae12f3dd403b9ae3376b737
Toby Rice <mr.skel@mail.ru>
2024-12-25 23:18:54 +0500

    refactor: moved kafka topics script

--- config/kafka-conf.sh
+++ config/kafka-conf.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-# Wait for Kafka to be ready
-until kafka-topics.sh --bootstrap-server localhost:9092 --list > /dev/null; do
-  echo "Waiting for Kafka..."
-  sleep 5
-done
+# kafka = "kafka:29092"
+      
+# blocks until kafka is reachable
+echo -e "Available kafka topics: "
+kafka-topics --bootstrap-server kafka:29092 --list
 
-echo "Creating Kafka topics..."
-
-# Create topics
 topics=(
   "email"
   "push"
   "telegram"
 )
 
+
+echo -e 'Creating kafka topics'
 for topic in "${topics[@]}"; do
   echo "Creating topic: $topic"
-  kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 \
-    --replication-factor 1 --partitions 1 --topic "$topic"
+  kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic "$topic" --replication-factor 1 --partitions 1
 done
 
-echo "Topics created successfully:"
-kafka-topics.sh --bootstrap-server localhost:9092 --list
+echo -e 'Successfully created the following topics:'
+kafka-topics --bootstrap-server kafka:29092 --list
+      
