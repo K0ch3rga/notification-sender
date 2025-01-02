4d00261cfc9eabda1edfa78ad15a0eba0c9dacc9
Nick Nepochatyy <nnepochatyy@mail.ru>
2025-01-02 05:49:01 +0500

    fix attempt: app creation failure

--- push/app/interfaces/api.py
+++ push/app/interfaces/api.py
@@ -1,17 +1,13 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
 from push.app.domain.services import NotificationService
 from push.app.infrastructure.database import db
-from push.app.infrastructure.push_service import PushService
 from push.app.domain.repositories import NotificationRepository
 
 bp = Blueprint('api', __name__)
 
-notification_repository = NotificationRepository(db)
-push_service = PushService(None)
-notification_service = NotificationService(notification_repository, push_service)
-
 @bp.route('/send', methods=['POST'])
 def send_notification():
+    notification_service = current_app.notification_service
     data = request.json
     notification_type = data.get('Type')
     address = data.get('Address')
--- push/run.py
+++ push/run.py
@@ -1,7 +1,22 @@
 from push.app import create_app
 from flask import render_template
+from push.app.infrastructure.database import db
+from push.app.infrastructure.push_service import PushService
+from push.app.domain.repositories import NotificationRepository
+from push.app.domain.services import NotificationService
+from push.app.interfaces.api import bp
 
 app = create_app()
+
+push_service = PushService(app)
+notification_repository = NotificationRepository(db)
+notification_service = NotificationService(notification_repository, push_service)
+
+@app.context_processor
+def inject_notification_service():
+    return dict(notification_service=notification_service)
+
+app.register_blueprint(bp)
 
 @app.route('/push')
 def push():
