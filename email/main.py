a4744445f994aedb7df734323cf6dfcd2acf22e5
Toby Rice <mr.skel@mail.ru>
2025-01-04 13:22:54 +0500

    fix: logging

--- email/main.py
+++ email/main.py
@@ -6,26 +6,25 @@ from src.Infrastructure.services.mail_service import send_email
 from src.Infrastructure.configs.config import TO_EMAIL, FROM_EMAIL, PSSWD
 from src.Infrastructure.services.kafkaService import consume_messages
 from src.DomainOrModels.Mail import Email
+
 # from querries.orm import create_tables,insert_mail
 from src.Infrastructure.services.kafkaService import consumer
 
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-topic = 'email'
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+topic = "email"
 
+print("Сервис запущен")
 
-while True:
-    try:
-        msg = consume_messages(topic)
+try:
+    while True:
+        msg = consume_messages()
+        if msg is None:
+            continue
         create_tables()
-        email = Email(msg["address"],
-                      FROM_EMAIL,
-                      msg["title"],
-                      msg["message"])
-        send_email(
-            email
-        )
-    except KeyboardInterrupt:
-        pass
-    finally:
-        consumer.close()
-
+        email = Email(msg["address"], FROM_EMAIL, msg["title"], msg["message"])
+        send_email(email)
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()
+    print("Сервис остановлен")
--- email/src/Infrastructure/services/kafkaService.py
+++ email/src/Infrastructure/services/kafkaService.py
@@ -1,23 +1,22 @@
-
 from confluent_kafka import Consumer
 import json
 
 conf = {
-    'bootstrap.servers': 'kafka:29092',  # Адрес Kafka-брокера
-    'group.id': 'python-consumer-group',  # Идентификатор группы потребителей
-    'auto.offset.reset': 'earliest'  # Начинать с самого раннего сообщения
+    "bootstrap.servers": "kafka:29092",  # Адрес Kafka-брокера
+    "group.id": "python-consumer-group",  # Идентификатор группы потребителей
+    "auto.offset.reset": "earliest",  # Начинать с самого раннего сообщения
 }
 
 consumer = Consumer(conf)
+consumer.subscribe(["email"])
 
-def consume_messages(topic):
 
-    consumer.subscribe([topic])
+def consume_messages():
+
     msg = consumer.poll(timeout=1.0)  # Ожидание сообщения с таймаутом
 
     if not msg is None and not msg.error():
         print(
-            f'Received message: {msg.value().decode("utf-8")} from {msg.topic()} [{msg.partition()}] at offset {msg.offset()}')
-        return json.loads(msg.value().decode('utf-8'))
-
-
+            f'Received message: {msg.value().decode("utf-8")} from {msg.topic()} [{msg.partition()}] at offset {msg.offset()}'
+        )
+        return json.loads(msg.value().decode("utf-8"))
